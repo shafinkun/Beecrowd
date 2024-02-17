266d5def9cecf79329227d484c6ee61ec6f985d8
Shahriar Hasnat Shafin Ahmed <126312684+shafinkun@users.noreply.github.com>
2024-02-17 19:30:57 +0600

    Update 1013.cpp

--- 1013.cpp
+++ 1013.cpp
@@ -1,13 +1,14 @@
-#include<iostream>                                                                                                                                          
-    using namespace std;
-    int main(){
+#include<bits/stdc++.h>                                                                                                                                          
+using namespace std;
+int main()
+{
         int a,b,c,greater;
-        cin>>a>>b>>c;
+        cin >> a >> b >> c;
         if(a>b)
-        {greater=std::max(a,c);}
+        greater = max(a,c);}
         else 
-        {greater=std::max(b,c);}
-        cout<<greater<<endl;
+        greater = max(b,c);}
+        cout << greater << endl;
         
         return 0;
-    }
+}
