b09bbc85ac777e3950cc723375a4b7de91c8fcc3
Shahriar Hasnat Shafin Ahmed <126312684+shafinkun@users.noreply.github.com>
2024-02-25 23:12:44 +0600

    Update 1019.cpp

--- 1019.cpp
+++ 1019.cpp
@@ -2,12 +2,13 @@
 using namespace std;
 int main()
 {
-    int x,sec,min,hr;
-    cin>>x;
-    sec=x%60;
-    x=x/60;
-    min=x%60;
-    hr=x/60;
-    cout<<hr<<":"<<min<<":"<<sec<<endl;
+    int x, sec, min, hr;
+    cin >> x;
+    sec = x % 60;
+    x = x / 60;
+    min = x % 60;
+    hr = x / 60;
+    cout << hr << ":" << min << ":" << sec << endl;
+
     return 0;
 }
